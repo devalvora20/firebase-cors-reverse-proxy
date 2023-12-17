f533b300b3c6fc60b81b36be65d59ccbf6e89f42
devalvora20 <devalvora1996@gmail.com>
2023-12-17 01:06:36 -0800

    initial commit
    with this code deployed on firebase, anyone can call this to get rid of cors issue. 
    there is a limit of 2million invocations in a month after which it will start charging

---
+++ index.js
@@ -0,0 +1,17 @@
+const { onRequest } = require('firebase-functions/v2/https');
+const corsAnywhere = require('cors-anywhere');
+const cors = require('cors');
+
+const corsServer = corsAnywhere.createServer({
+    originWhitelist: [], // allow every domain to call this 
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+});
+
+const corsHandler = cors({ origin: true });
+
+exports.proxy = onRequest({ maxInstances: 10 }, (request, response) => {
+    corsHandler(request, response, () => {
+      corsServer.emit('request', request, response);
+    })
+});
